6b57dc8e2503c866582bb64404f930f91d11b7c1
José Palma <palmahn@gmail.com>
2020-07-19 13:44:18 -0600

    Creación de la conexión a MongoDB(local).

--- src/main/java/dev/jcpalma/hero/api/RestApplication.java
+++ src/main/java/dev/jcpalma/hero/api/RestApplication.java
@@ -1,7 +1,12 @@
 package dev.jcpalma.hero.api;
 
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoDatabase;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+
 
 /**
  * Configures JAX-RS for the application.
@@ -9,5 +14,13 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("api")
 public class RestApplication extends Application {
+    public static MongoClient mongo;
     
+    static {
+        mongo = MongoClients.create();
+    }
+    
+    public RestApplication() {
+        MongoDatabase admin = mongo.getDatabase("local");
+    }
 }
